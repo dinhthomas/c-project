e18bf393762fc8caf0a9ab25a953d382e72f82e4
Thomas Dinh Hoang <thomass@Thomass-Air.fritz.box>
2020-05-06 22:45:58 +0200

    Debug Tracer

--- loesung.c
+++ loesung.c
@@ -1,12 +1,26 @@
 
 //Header 
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h> 
+
+
+#if !defined (NDEBUG) 
+   #define TRACE(MSG) \
+        fprintf(stderr, MSG)
+#else
+#define TRACE(MSG) 
+#endif
+
+
+
 
 
 
 
 int main() {
    // printf() displays the string inside quotation
+   TRACE("Main Method ");
    printf("Hello, World!");
    return 0;
 }
